92d1ba88f9c28d5934c93cd707725828f7fa2139
Petr Lozhchenko <firefly.app.group@gmail.com>
2018-08-03 21:52:17 +0300

    Adding UnitOfWork pattern

---
+++ CityTowerServer/CityTowerServer.DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using CityTowerServer.DAL.Models;
+using CityTowerServer.DAL.Repository;
+using System;
+
+namespace CityTowerServer.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<NewsCategoryList> NewsRepository { get; }
+        void Save();
+    }
+}
---
+++ CityTowerServer/CityTowerServer.DAL/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CityTowerServer.DAL.Models;
+using CityTowerServer.DAL.Repository;
+
+namespace CityTowerServer.DAL.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly CityTower_bdContext _context;
+        private IGenericRepository<NewsCategoryList> newsRepository;
+
+        public UnitOfWork()
+        {
+            this._context = new CityTower_bdContext();
+        }
+
+        public IGenericRepository<NewsCategoryList> NewsRepository
+        {
+            get { return this.newsRepository ?? (this.newsRepository = new GenericRepository<NewsCategoryList>(_context)); }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
