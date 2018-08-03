cf283d313d51c21b429047e95ffe1b0235489b7c
Petr Lozhchenko <firefly.app.group@gmail.com>
2018-08-03 21:49:22 +0300

    Adding Generic Repository

---
+++ CityTowerServer/CityTowerServer.DAL/Repository/GenericRepository.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CityTowerServer.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityTowerServer.DAL.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private CityTower_bdContext _entities;
+
+        public GenericRepository(CityTower_bdContext context)
+        {
+            _entities = context;
+        }
+
+        public CityTower_bdContext database
+        {
+            get { return _entities; }
+            set { _entities = value; }
+        }
+
+        public virtual List<T> GetAll()
+        {
+            IQueryable<T> query = _entities.Set<T>();
+            return query.ToList();
+        }
+
+        public List<T> FindBy(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            IQueryable<T> query = _entities.Set<T>().Where(predicate);
+            return query.ToList();
+        }
+        public virtual void Attach(T entity)
+        {
+            _entities.Set<T>().Attach(entity);
+        }
+
+        public virtual bool Add(T entity)
+        {
+            _entities.Set<T>().Add(entity);
+            return true;
+        }
+
+        public virtual bool Delete(T entity)
+        {
+            _entities.Set<T>().Remove(entity);
+            return true;
+        }
+
+        public virtual bool Edit(T entity)
+        {
+            _entities.Entry(entity).State = EntityState.Modified;
+            return true;
+        }
+
+        public virtual bool Save()
+        {
+            _entities.SaveChanges();
+            return true;
+        }
+
+        public virtual bool SaveChanges(T entity)
+        {
+            if (_entities.Entry(entity).State == EntityState.Detached)
+            {
+                _entities.Set<T>().Attach(entity);
+            }
+            _entities.Entry(entity).State = EntityState.Modified;
+            _entities.SaveChanges();
+            return true;
+        }
+
+        public virtual T FindById(int id)
+        {
+            return _entities.Set<T>().Find(id);
+        }
+
+        public T Get(int id)
+        {
+            return _entities.Set<T>().Find(id);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _entities.Set<T>().AddRange(entities);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _entities.Set<T>().RemoveRange(entities);
+        }
+
+        public void Update(T item)
+        {
+            _entities.Entry(item).State = EntityState.Modified;
+        }
+    }
+}
